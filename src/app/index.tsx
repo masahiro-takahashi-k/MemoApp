cb283961a45315767cdd9da0f52c05e37e28c1e4
masahiro <102397276+masahiro-takahashi-k@users.noreply.github.com>
2025-11-25 09:35:13 +0900

    Install expo-router

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native'
+
+const Index = (): JSX.Element => { //Indexページの定義
+    return (
+        <View>
+            <Text>Hello World</Text>
+        </View>
+    )
+}
+
+export default Index
